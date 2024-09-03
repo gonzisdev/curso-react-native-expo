eae8a570d2cee03d5d8808285bd001710dbfe542
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-03 14:37:16 +0200

    navigation between screens

--- 5-tenedores/src/screens/Restaurants/RestaurantsScreen.tsx
+++ 5-tenedores/src/screens/Restaurants/RestaurantsScreen.tsx
@@ -1,9 +1,23 @@
 import { View, Text } from "react-native"
+import { Button } from "@rneui/base"
+import { screen } from "../../utils/screenName"
 
-export const RestaurantsScreen = () => {
+type RestaurantsScreenProps = {
+  navigation: {
+    navigate: (routeName: string) => void
+  }
+}
+
+export const RestaurantsScreen = ({navigation}: RestaurantsScreenProps) => {
+  
+  const goToAddRestaurant = () => {
+    navigation.navigate(screen.restaurant.addRestaurant)
+  }
+
   return (
     <View>
         <Text>Estamos en la screen Restaurants</Text>
+        <Button title="Crear restaurante" onPress={goToAddRestaurant} />
     </View>
   )
 }
