445e97c46a7b27622c55d2bb8628542a29fba59d
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-04 16:43:06 +0200

    keyboard aware scroll view in registerformscren

--- 5-tenedores/src/screens/Account/RegisterScreen/RegisterScreen.tsx
+++ 5-tenedores/src/screens/Account/RegisterScreen/RegisterScreen.tsx
@@ -2,14 +2,15 @@ import { View } from 'react-native'
 import { Image } from '@rneui/base'
 import { styles } from './RegisterScreen.styles'
 import { RegisterForm } from '../../../components/Auth/RegisterForm/RegisterForm'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export const RegisterScreen = () => {
   return (
-    <View>
+    <KeyboardAwareScrollView>
       <Image source={require("../../../../assets/img/5-tenedores-letras-icono-logo.png")} style={styles.image} />
       <View style={styles.content}>
         <RegisterForm />
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   )
 }
