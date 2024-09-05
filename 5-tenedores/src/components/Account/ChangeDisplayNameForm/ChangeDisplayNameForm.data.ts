8fd6d62b1072a7058352d865ad224f57d74eccea
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-05 17:10:45 +0200

    changedisplaynameform validation

---
+++ 5-tenedores/src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.data.ts
@@ -0,0 +1,14 @@
+import * as Yup from "yup"
+
+export const initialValues = () => {
+    return {
+        displayName: '',
+    }
+}
+
+export const validationSchema = () => {
+    return Yup.object({
+        displayName: Yup.string()
+            .required("El nombre y los apellidos son obligatorios")
+    })
+}
--- 5-tenedores/src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.tsx
+++ 5-tenedores/src/components/Account/ChangeDisplayNameForm/ChangeDisplayNameForm.tsx
@@ -1,16 +1,43 @@
 import { View } from 'react-native'
 import { Input, Button } from '@rneui/base'
 import { styles } from './ChangeDisplayNameForm.styles'
+import { useFormik } from 'formik'
+import { initialValues, validationSchema } from './ChangeDisplayNameForm.data'
 
 type ChangeDisplayNameFormProps = {
     onClose: () => void
 }
 
 export const ChangeDisplayNameForm = ({onClose}: ChangeDisplayNameFormProps) => {
+
+    const formik = useFormik({
+        initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
+        onSubmit: (formValue) => {
+
+        }
+    })
+
+    const handleSubmit = () => {
+        formik.handleSubmit();
+      }
+
   return (
     <View style={styles.content}>
-      <Input placeholder='Nombre y apellidos' rightIcon={{type: "material-community", name:"account-circle-outline", color: "#c2c2c2"}} />
-      <Button title="Cambiar nombre y apellidos" containerStyle={styles.btnContainer} buttonStyle={styles.btn} />
+      <Input 
+        placeholder='Nombre y apellidos' 
+        rightIcon={{type: "material-community", name:"account-circle-outline", color: "#c2c2c2"}} 
+        onChange={text => formik.setFieldValue('displayName', text)}
+        errorMessage={formik.errors.displayName}
+      />
+      <Button 
+        title="Cambiar nombre y apellidos" 
+        containerStyle={styles.btnContainer} 
+        buttonStyle={styles.btn} 
+        onPress={handleSubmit} 
+        loading={formik.isSubmitting} 
+      />
     </View>
   )
 }
