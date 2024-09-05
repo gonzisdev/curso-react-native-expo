fdd42c5d327e220480f94d24b8d9ec171eb2e5ea
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-05 11:57:19 +0200

    show and hiding password in login form

--- 5-tenedores/src/components/Auth/LoginForm/LoginForm.tsx
+++ 5-tenedores/src/components/Auth/LoginForm/LoginForm.tsx
@@ -1,8 +1,14 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View } from 'react-native'
 import { Input, Icon, Button } from '@rneui/base'
 import { styles } from './LoginForm.styles'
 
 export const LoginForm = () => {
+    
+    const [showPassword, setShowPassword] = useState(false)
+
+    const showHiddenPassword = () => setShowPassword(prevState => !showPassword)
+
   return (
     <View style={styles.content}>
       <Input 
@@ -13,8 +19,8 @@ export const LoginForm = () => {
       <Input 
         placeholder='Contraseña' 
         containerStyle={styles.input} 
-        secureTextEntry={true} 
-        rightIcon={<Icon type='material-community' name='eye-outline' iconStyle={styles.icon}/>} 
+        secureTextEntry={showPassword ? false : true} 
+        rightIcon={<Icon type='material-community' name={showPassword ? 'eye-off-outline' : 'eye-outline'} iconStyle={styles.icon} onPress={showHiddenPassword} />} 
       />
       <Button title='Iniciar sesión' containerStyle={styles.btnContainer} buttonStyle={styles.btn} />
     </View>
