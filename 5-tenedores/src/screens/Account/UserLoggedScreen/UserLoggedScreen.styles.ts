c1d9f4bfed940b5e8f899268660311602ebe7d75
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-05 15:27:34 +0200

    logout

--- 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.styles.ts
+++ 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.styles.ts
@@ -1,3 +1,20 @@
 import { StyleSheet } from "react-native"
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+    btnStyles: {
+        marginTop: 30,
+        borderRadius: 0,
+        backgroundColor: "#fff",
+        borderTopWidth: 1,
+        borderTopColor: "#e3e3e3",
+        borderBottomWidth: 1,
+        borderBottomColor: "#e3e3e3"
+    },
+    btn: {
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+    },
+    btnTextStyles: {
+        color: "#00a680"
+    }
+})
--- 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.tsx
+++ 5-tenedores/src/screens/Account/UserLoggedScreen/UserLoggedScreen.tsx
@@ -1,11 +1,26 @@
 import { View } from 'react-native'
+import { Button } from '@rneui/base'
 import { InfoUser } from '../../../components/Account/InfoUser/InfoUser'
 import { styles } from './UserLoggedScreen.styles'
+import { getAuth, signOut } from 'firebase/auth'
 
 export const UserLoggedScreen = () => {
+
+  const logout = async () => {
+    const auth = getAuth()
+    await signOut(auth)
+  }
+
   return (
     <View>
       <InfoUser />
+      <Button 
+        title="Cerrar sesión" 
+        containerStyle={styles.btnStyles}
+        buttonStyle={styles.btn} 
+        titleStyle={styles.btnTextStyles} 
+        onPress={logout}
+      />
     </View>
   )
 }
