2f580743bb53ca9fd7a29ad3b746e7fb8e9b68d1
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-06 11:28:46 +0200

    location icon color state

--- 5-tenedores/src/components/Restaurants/AddRestaurant/InfoForm/InfoForm.tsx
+++ 5-tenedores/src/components/Restaurants/AddRestaurant/InfoForm/InfoForm.tsx
@@ -30,7 +30,7 @@ export const InfoForm = ({formik}: InfoFormProps) => {
           rightIcon={{
               type: "material-community",
               name: "map-marker-radius",
-              color: "#c2c2c2",
+              color: getColorIconMap(formik),
               onPress: onOpenCloseMap
           }}
           onChangeText={text => formik.setFieldValue('address', text)}
@@ -57,4 +57,10 @@ export const InfoForm = ({formik}: InfoFormProps) => {
       <MapForm show={showMap} close={onOpenCloseMap} formik={formik} />
     </>
   )
+}
+
+const getColorIconMap = (formik: FormikProps<any>) => {
+  if(formik.errors.location) return "#ff0000"
+  if(formik.values.location) return "#00a680"
+  return "#c2c2c2"
 }
