bd952b251cd121827d8ca8b561a2f0bfe0b7ae0a
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-07 13:01:41 +0200

    pagination carousel

--- 5-tenedores/src/components/Shared/Carousel/Carousel.tsx
+++ 5-tenedores/src/components/Shared/Carousel/Carousel.tsx
@@ -1,5 +1,6 @@
+import { useState } from 'react'
 import { View } from 'react-native'
-import CarouselSnap from "react-native-snap-carousel"
+import CarouselSnap, { Pagination } from "react-native-snap-carousel"
 import { Image } from '@rneui/base'
 import { styles } from './Carousel.styles'
 
@@ -7,13 +8,29 @@ type CarouselProps = {
     arrayImages: string[]
     height: number
     width: number
+    hideDots?: boolean
 }
 
-export const Carousel = ({arrayImages, width, height}: CarouselProps) => {
+export const Carousel = ({arrayImages, width, height, hideDots}: CarouselProps) => {
+
+    const [activeDotIndex, setActiveDotIndex] = useState(0)
 
     const renderItem =  ({ item }: { item: string }) => (
         <Image source={{uri: item}} style={{height, width}}  />
     )
+
+    const pagination = () => {
+      return (
+        <Pagination 
+          dotsLength={arrayImages.length}
+          activeDotIndex={activeDotIndex}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.6}
+          containerStyle={styles.dotContainer}
+          dotStyle={styles.dot}
+        />
+      )
+    }
 
   return (
     <View style={styles.content}>
@@ -23,7 +40,9 @@ export const Carousel = ({arrayImages, width, height}: CarouselProps) => {
         sliderWidth={width}
         itemWidth={width}
         renderItem={renderItem}
+        onSnapToItem={(index) => setActiveDotIndex(index)}
       />
+      {!hideDots && pagination()}
     </View>
   )
 }
