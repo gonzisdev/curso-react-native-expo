1c58fa588c5275988128c21d80e0c8dbd2f83158
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-08 17:58:37 +0200

    adding to favorites

--- 5-tenedores/src/components/Restaurant/BtnFavorite/BtnFavorite.tsx
+++ 5-tenedores/src/components/Restaurant/BtnFavorite/BtnFavorite.tsx
@@ -1,15 +1,30 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import { Icon } from '@rneui/base'
+import { getAuth } from 'firebase/auth'
+import { doc, getDocs, setDoc, query, where, collection, deleteDoc } from 'firebase/firestore'
+import { db } from '../../../utils/firebase'
+import { v4 as uuid } from 'uuid'
 import { styles } from './BtnFavorite.styles'
 
 type BtnFavoriteProps = {
     idRestaurant: string // Mejor crear tipo de Restaurant de manera global y hacer look up
 }
 
-export const BtnFavorite = ({idRestaurant}) => {
+export const BtnFavorite = ({idRestaurant}: BtnFavoriteProps) => {
 
-    const addFavorite = () => {
-
+    const auth = getAuth()
+    const addFavorite = async () => {
+        try {
+            const idFavorite = uuid()
+            const data = {
+                id: idFavorite,
+                idRestaurant,
+                idUser: auth.currentUser?.uid
+            }
+            await setDoc(doc(db, "favorites", idFavorite), data)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
   return (
