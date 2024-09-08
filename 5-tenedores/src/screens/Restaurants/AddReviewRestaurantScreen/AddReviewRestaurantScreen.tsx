8183aae5fe5a1bf656e45933e371ba7280288c2b
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-08 15:20:30 +0200

    add review form

--- 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.tsx
+++ 5-tenedores/src/screens/Restaurants/AddReviewRestaurantScreen/AddReviewRestaurantScreen.tsx
@@ -1,4 +1,5 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { AirbnbRating, Input, Button } from '@rneui/themed'
 import { styles } from './AddReviewRestaurantScreen.styles'
 
 type AddReviewRestaurantScreenProps = { // No hacer esto xD Tipar correctamente
@@ -11,8 +12,23 @@ type AddReviewRestaurantScreenProps = { // No hacer esto xD Tipar correctamente
 
 export const AddReviewRestaurantScreen = ({route}: AddReviewRestaurantScreenProps) => {
   return (
-    <View>
-      <Text>AddReviewRestaurantScreen</Text>
+    <View style={styles.content}>
+      <View>
+        <View style={styles.ratingContent}>
+            <AirbnbRating 
+                count={5} 
+                reviews={["Pésimo", "Deficiente", "Normal", "Muy bueno", "Excelente"]} 
+                defaultRating={0}
+                size={35}
+                onFinishRating={(rating) => console.log(rating)}
+            />
+        </View>
+        <View>
+            <Input placeholder='Título' />
+            <Input placeholder='Comentario' multiline inputContainerStyle={styles.comment} />
+        </View>
+      </View>
+      <Button title="Enviar review" containerStyle={styles.btnContainer} buttonStyle={styles.btn} />
     </View>
   )
 }
