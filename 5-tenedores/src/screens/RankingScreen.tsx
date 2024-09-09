cbf8ec7d169d5f749f1a37bc14063f80376a0406
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-09 12:43:35 +0200

    getting ranked restaurants

--- 5-tenedores/src/screens/RankingScreen.tsx
+++ 5-tenedores/src/screens/RankingScreen.tsx
@@ -1,6 +1,23 @@
+import { useState, useEffect } from 'react'
 import { View, Text } from 'react-native'
+import { collection, query, orderBy, onSnapshot, limit, QueryDocumentSnapshot, DocumentData } from 'firebase/firestore'
+import { db } from '../utils/firebase'
 
 export const RankingScreen = () => {
+
+  const [restaurants, setRestaurants] = useState<QueryDocumentSnapshot<DocumentData, DocumentData>[]>([])
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "restaurants"),
+      orderBy("ratingMedia", "desc"),
+      limit(10)
+    )
+    onSnapshot(q, (snapshot) => {
+      setRestaurants(snapshot.docs)
+    })
+  }, [])
+
   return (
     <View>
       <Text>RankingScreen</Text>
