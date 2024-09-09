f4a373e80683d553935b96344ad30af2cbe9038e
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-09 12:35:51 +0200

    listing restaurantes in favorits screen

---
+++ 5-tenedores/src/components/Favorites/RestaurantFavorite/RestaurantFavorite.tsx
@@ -0,0 +1,43 @@
+import { View, TouchableOpacity } from 'react-native'
+import { Image, Icon, Text } from '@rneui/base'
+import { DocumentData } from 'firebase/firestore'
+import { styles } from './RestaurantFavorite.styles'
+import { useNavigation } from '@react-navigation/native'
+import { screen } from '../../../utils/screenName'
+
+
+type RestaurantFavoriteProps = {
+    restaurant: DocumentData // No hacer esto, tipar restaurant correctamente de manera global
+}
+
+export const RestaurantFavorite = ({restaurant}: RestaurantFavoriteProps) => {
+
+    const navigation = useNavigation()
+
+    const goToRestaurant = () => {
+        navigation.navigate(screen.restaurant.tab, {
+            screen: screen.restaurant.restaurant,
+            params: {
+                id: restaurant.id
+            } // No hacer esto xD, tipar correctamente
+        })
+    }
+
+    const onRemoveFavorite = () => {
+
+    }
+
+  return (
+    <TouchableOpacity onPress={goToRestaurant}>
+      <View style={styles.content}>
+        <Image source={{uri: restaurant.images[0]}} style={styles.image} />
+        <View style={styles.infoContent}>
+            <Text style={styles.name}>
+                {restaurant.name}
+            </Text>
+            <Icon type='material-community' name='heart' color="#f00" size={35} containerStyle={styles.iconContainer} onPress={onRemoveFavorite} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
--- 5-tenedores/src/screens/FavoritesScreen.tsx
+++ 5-tenedores/src/screens/FavoritesScreen.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
-import { View, Text } from "react-native"
+import { Text, ScrollView } from "react-native"
 import { UserNotLogged } from "../components/Favorites/UserNotLogged/UserNotLogged"
 import { NotFoundRestaurants } from "../components/Favorites/NotFoundRestaurants/NotFoundRestaurants"
 import { Loading } from "../components/Shared/Loading/Loading"
 import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { doc, getDoc, collection, query, where, onSnapshot, DocumentData } from "firebase/firestore"
 import { db } from "../utils/firebase"
+import { RestaurantFavorite } from "../components/Favorites/RestaurantFavorite/RestaurantFavorite"
 
 export const FavoritesScreen = () => {
 
@@ -45,10 +46,12 @@ export const FavoritesScreen = () => {
   if (!hasLogged) return <UserNotLogged />
   if (!restaurants) return <Loading show text="Cargando" />
   if (restaurants.length === 0) return <NotFoundRestaurants />
-
+  
   return (
-    <View>
-        <Text>FavoritesScreen</Text>
-    </View>
+    <ScrollView>
+        {restaurants.map(restaurant => (
+          <RestaurantFavorite key={restaurant.id} restaurant={restaurant} />
+        ))}
+    </ScrollView>
   )
 }
