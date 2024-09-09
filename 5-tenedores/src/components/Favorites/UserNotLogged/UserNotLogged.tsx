08bf45208fce810c6d0011e6c66a3d1252882103
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-09-09 10:58:06 +0200

    userNotLogged screen

---
+++ 5-tenedores/src/components/Favorites/UserNotLogged/UserNotLogged.tsx
@@ -0,0 +1,26 @@
+import { View } from 'react-native'
+import { Text, Icon, Button } from '@rneui/base'
+import { useNavigation } from '@react-navigation/native'
+import { screen } from '../../../utils/screenName'
+import { styles } from './UserNotLogged.styles'
+
+export default function UserNotLogged() {
+
+    const navigation = useNavigation()
+
+    const goToLogin = () => {
+        navigation.navigate(screen.account.tab, {
+            screen: screen.account.login
+        }) // No hacer esto xD Tipar correctamente
+    }
+
+  return (
+    <View style={styles.content}>
+      <Icon type='material-community' name='alert-outline' size={80} />
+      <Text style={styles.info}>
+        Necesitas estar logeado para ver esta sección
+      </Text>
+      <Button title="Ir al login" containerStyle={styles.btnContainer} buttonStyle={styles.btn} onPress={goToLogin} />
+    </View>
+  )
+}
--- 5-tenedores/src/screens/FavoritesScreen.tsx
+++ 5-tenedores/src/screens/FavoritesScreen.tsx
@@ -1,6 +1,22 @@
+import { useState, useEffect } from "react"
 import { View, Text } from "react-native"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import UserNotLogged from "../components/Favorites/UserNotLogged/UserNotLogged"
 
 export const FavoritesScreen = () => {
+
+  const [hasLogged, setHasLogged] = useState<boolean | null>(null)
+  const auth = getAuth()
+
+  useEffect(() => {
+    const auth = getAuth()
+    onAuthStateChanged(auth, (user) => {
+      setHasLogged(user ? true : false)
+    })
+  }, [])
+
+  if (!hasLogged) return <UserNotLogged />
+
   return (
     <View>
         <Text>FavoritesScreen</Text>
